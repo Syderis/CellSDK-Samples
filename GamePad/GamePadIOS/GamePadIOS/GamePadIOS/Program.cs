a3b868b7c29098d03eb9df72c40dae5303416311
Jorge Cantón Ferrero <jcanton@syderis.com>
2011-10-25 15:09:58 +0200

    GamePadIOS project

---
+++ GamePad/GamePadIOS/GamePadIOS/GamePadIOS/Program.cs
@@ -0,0 +1,29 @@
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Syderis.CellSDK.IOS.Launcher;
+
+namespace GamePad
+{
+	[Register("AppDelegate")]
+	class Program : UIApplicationDelegate
+	{
+		public static Program Instance;
+		
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+
+		public override void FinishedLaunching (UIApplication app)
+		{
+			Instance = this;
+			Application application = new Application ();
+			Kernel kernel = new Kernel (application);
+			kernel.Run ();
+		}
+		
+		public void Exit()
+		{}
+	}
+}
