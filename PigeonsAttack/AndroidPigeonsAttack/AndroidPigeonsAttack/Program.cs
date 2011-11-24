c1a3592605b2b4f217e49675da091057145c5dd0
Marcos Cobena Morian <marcoscm@me.com>
2011-11-24 16:33:02 +0100

    PigeonsAttack for Android

---
+++ PigeonsAttack/AndroidPigeonsAttack/AndroidPigeonsAttack/Program.cs
@@ -0,0 +1,48 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Syderis.CellSDK.Android.Launcher;
+using Syderis.CellSDK.Common;
+using Syderis.CellSDK.Core;
+
+namespace PigeonsAttack
+{
+    [Activity(Label = "AndroidPigeonsAttack", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Program : Activity
+    {
+        public static Program Instance;
+
+        /// <summary>
+        /// The main method which loads Application.
+        /// </summary>
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Kernel view = new Kernel(this);
+            SetContentView(view.Window);
+
+            Instance = this;
+            Preferences.SkinXMLFileStream = Assets.Open("Content/Skin/Skin.xml");
+            Preferences.ApplicationActivity = this;
+
+            Application application = new Application();
+            view.Application = application;
+            view.Run();
+        }
+
+        /// <summary>
+        /// Exit Method.
+        /// </summary>
+        public void Exit()
+        {
+            Finish();
+        }
+    }
+}
