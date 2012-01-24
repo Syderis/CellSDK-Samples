267c74ca1473bd7011f9e9eec8befce075826229
jfernandez <jfernandez@syderis.com>
2012-01-24 17:56:23 +0100

    Fixed Android ShareTheBill Sample

--- ShareTheBill/AndroidShareTheBill/ShareTheBillAndroid/Program.cs
+++ ShareTheBill/AndroidShareTheBill/ShareTheBillAndroid/Program.cs
@@ -1,20 +1,15 @@
-﻿using System;
-
+﻿#region Using Statements
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
+using Microsoft.Xna.Framework;
 using Syderis.CellSDK.Android.Launcher;
 using Syderis.CellSDK.Common;
-using Syderis.CellSDK.Core;
+#endregion
 
 namespace ShareBill
 {
     [Activity(Label = "AndroidAccelerometer", MainLauncher = true, Icon = "@drawable/icon")]
-    public class Program : Activity
+    public class Program : AndroidGameActivity
     {
         public static Program Instance;
 
@@ -25,6 +20,7 @@ namespace ShareBill
         {
             base.OnCreate(savedInstanceState);
 
+            Kernel.Activity = this;
             Kernel view = new Kernel(this);
             SetContentView(view.Window);
 
