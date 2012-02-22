ba213200b555f1bb14f3544f5260cb8deeefad0b
Jorge Cantón Ferrero <jcanton@syderis.com>
2012-02-22 15:39:37 +0100

    Update to v1.1 IOS

--- ScreenManager/IOSScreenManager/IOSScreenManager/Program.cs
+++ ScreenManager/IOSScreenManager/IOSScreenManager/Program.cs
@@ -1,7 +1,13 @@
+/*
+ * Copyright 2012 Syderis Technologies S.L. All rights reserved.
+ * Use is subject to license terms.
+ */
+
+#region Using Statements
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
-
 using Syderis.CellSDK.IOS.Launcher;
+#endregion
 
 namespace ScreenManager
 {
@@ -9,7 +15,8 @@ namespace ScreenManager
 	class Program : UIApplicationDelegate
 	{
 		public static Program Instance;
-
+		private Kernel kernel;
+		
 		static void Main (string[] args)
 		{
 			UIApplication.Main (args, null, "AppDelegate");
@@ -17,15 +24,33 @@ namespace ScreenManager
 
 		public override void FinishedLaunching (UIApplication app)
 		{
-			Instance=this;
+			Instance = this;
 			Application application = new Application ();
-			Kernel kernel = new Kernel (application);
+			kernel = new Kernel (application);
 			kernel.Run ();
 		}
 
-		public void Exit()
+		public void Exit ()
 		{
-			Instance.Exit();
+			kernel.Exit ();
+		}
+		
+		public override void DidEnterBackground (UIApplication application)
+		{
+			kernel.OnDeactivated ();
+		}
+		
+		public override void WillEnterForeground (UIApplication application)
+		{
+			kernel.OnActivated ();
+		}
+		
+		public override void WillTerminate (UIApplication application)
+		{
+			kernel.OnExiting ();
 		}
 	}
 }
+
+
+
