5e1275bd8676baf51bfdb50a32caab08428371b2
Jorgemagic <jcanton@syderis.com>
2012-02-22 14:14:04 +0100

    - Update to v1.1 Android

--- TwitterSearch/AndroidTwitterSearch/AndroidTwitterSearch/Program.cs
+++ TwitterSearch/AndroidTwitterSearch/AndroidTwitterSearch/Program.cs
@@ -1,17 +1,17 @@
-using System;
+/*
+ * Copyright 2012 Syderis Technologies S.L. All rights reserved.
+ * Use is subject to license terms.
+ */
 
+#region Using Statements
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 using Microsoft.Xna.Framework;
 
 using Syderis.CellSDK.Android.Launcher;
-using Syderis.CellSDK.Common;
-using Syderis.CellSDK.Core;
+using Syderis.CellSDK.Common; 
+#endregion
 
 namespace TwitterSearch
 {
@@ -19,6 +19,7 @@ namespace TwitterSearch
     public class Program : AndroidGameActivity
     {
         public static Program Instance;
+        private Kernel kernel;
 
         /// <summary>
         /// The main method which loads Application.
@@ -29,16 +30,18 @@ namespace TwitterSearch
 
             Kernel.Activity = this;
 
-            Kernel view = new Kernel(this);
-            SetContentView(view.Window);
+            kernel = new Kernel(this);
+            SetContentView(kernel.Window);
 
             Instance = this;
-            Preferences.SkinXMLFileStream = Assets.Open("Content/Skin/Skin.xml");
-            Preferences.ApplicationActivity = this;
+            Syderis.CellSDK.Common.Preferences.SkinXMLFileStream = Assets.Open("Content/Skin/Skin.xml");
+            Syderis.CellSDK.Common.Preferences.ApplicationActivity = this;
+
+
 
             Application application = new Application();
-            view.Application = application;
-            view.Run();
+            kernel.Application = application;
+            kernel.Run();
         }
 
         /// <summary>
@@ -47,6 +50,26 @@ namespace TwitterSearch
         public void Exit()
         {
             Finish();
+        }
+
+        /// <summary>
+        /// The application's activity pauses the execution
+        /// </summary>
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            kernel.OnPause();
+        }
+
+        /// <summary>
+        /// The application's activity resumes the execution
+        /// </summary>
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            kernel.OnResume();
         }
     }
 }
