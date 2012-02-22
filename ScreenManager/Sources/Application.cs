46b7a15078e6381e59818495de629d2676170171
Jorgemagic <jcanton@syderis.com>
2012-02-22 10:25:15 +0100

    - Delete old screenmanager

--- ScreenManager/Sources/Application.cs
+++ ScreenManager/Sources/Application.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Syderis Technologies S.L. All rights reserved.
+ * Copyright 2012 Syderis Technologies S.L. All rights reserved.
  * Use is subject to license terms.
  */
 
@@ -10,51 +10,27 @@ namespace ScreenManager
     /// <summary>
     /// Application class
     /// </summary>
-    class Application : MobileApplication
+    public class Application : MobileApplication
     {
-        #region Variables
-        ScreenManager screenManager; //ScreenManager Instance
-        #endregion
 
-        #region Public Methods
         /// <summary>
         /// The main method for loading controls and resources.
         /// </summary>
         public override void Initialize()
         {
             base.Initialize();
-        //Instantiates the screen manager
-            screenManager = new ScreenManager(this);
-
+      
         }
-
-        /// <summary>
-        /// Calls to screenmanager update method.
-        /// </summary>
-        /// <param name="gameTime"></param>
-        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
-        {
-            base.Update(gameTime);
-            screenManager.Update(gameTime);
-        }
-
-        /// <summary>
-        /// Release the resources from screen manager.
-        /// </summary>
-        public override void Dispose()
-        {
-            base.Dispose();
-            screenManager.Dispose();
-        }
-
+     
         /// <summary>
         /// Exits the application.
         /// </summary>
-        public override void BackButtonPressed()
+        public override void Exit()
         {
-            screenManager.BackButtonPressed();
+            base.Exit();
+
+            Program.Instance.Exit();
         }
 
-        #endregion
     }
 }
